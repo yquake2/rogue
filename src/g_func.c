b4363080c7ef4b68cb85b56832d8e23d6cd50f5c
Yamagi Burmeister <yamagi@yamagi.org>
2011-12-08 11:02:36 +0000

    Added a CHANGELOG

--- src/g_func.c
+++ src/g_func.c
@@ -436,8 +436,16 @@ void plat_blocked (edict_t *self, edict_t *other)
 		// give it a chance to go away on it's own terms (like gibs)
 		T_Damage (other, self, self, vec3_origin, other->s.origin, vec3_origin, 100000, 1, 0, MOD_CRUSH);
 		// if it's still there, nuke it
-		if (other && other->inuse)		// PGM
-			BecomeExplosion1 (other);
+		if (other)
+		{
+			/* Hack for entity without it's origin near the model */
+			vec3_t save;
+			VectorCopy(other->s.origin,save);
+			VectorMA (other->absmin, 0.5, other->size, other->s.origin);
+
+			BecomeExplosion1(other);
+		}
+
 		return;
 	}
 
@@ -1724,8 +1732,16 @@ void door_blocked  (edict_t *self, edict_t *other)
 		// give it a chance to go away on it's own terms (like gibs)
 		T_Damage (other, self, self, vec3_origin, other->s.origin, vec3_origin, 100000, 1, 0, MOD_CRUSH);
 		// if it's still there, nuke it
-		if (other && other->inuse)
-			BecomeExplosion1 (other);
+		if (other)
+		{
+			/* Hack for entitiy without their origin near the model */
+			vec3_t save;
+			VectorCopy(other->s.origin,save);
+			VectorMA (other->absmin, 0.5, other->size, other->s.origin);
+
+			BecomeExplosion1(other);
+		}
+
 		return;
 	}
 
@@ -2169,8 +2185,16 @@ void train_blocked (edict_t *self, edict_t *other)
 		// give it a chance to go away on it's own terms (like gibs)
 		T_Damage (other, self, self, vec3_origin, other->s.origin, vec3_origin, 100000, 1, 0, MOD_CRUSH);
 		// if it's still there, nuke it
-		if (other && other->inuse)
-			BecomeExplosion1 (other);
+        if (other)
+		{
+			/* Hack for entity without an origin near the model */			
+			vec3_t save;
+			VectorCopy(other->s.origin,save);
+			VectorMA (other->absmin, 0.5, other->size, other->s.origin);
+
+			BecomeExplosion1(other);
+		} 
+
 		return;
 	}
 
@@ -2695,8 +2719,16 @@ void door_secret_blocked  (edict_t *self, edict_t *other)
 		// give it a chance to go away on it's own terms (like gibs)
 		T_Damage (other, self, self, vec3_origin, other->s.origin, vec3_origin, 100000, 1, 0, MOD_CRUSH);
 		// if it's still there, nuke it
-		if (other && other->inuse)
-			BecomeExplosion1 (other);
+		if (other)
+		{
+			/* Hack for entities without their origin near the model */
+			vec3_t save;
+			VectorCopy(other->s.origin,save);
+			VectorMA (other->absmin, 0.5, other->size, other->s.origin);
+
+			BecomeExplosion1(other);
+		}
+
 		return;
 	}
 
@@ -2787,6 +2819,15 @@ Kills everything inside when fired, irrespective of protection.
 void use_killbox (edict_t *self, edict_t *other, edict_t *activator)
 {
 	KillBox (self);
+
+	/* Hack to make sure that really everything is killed */
+	self->count--;
+
+	if (!self->count) 
+	{
+		self->think = G_FreeEdict;
+		self->nextthink = level.time + 1;
+	}
 }
 
 void SP_func_killbox (edict_t *ent)
