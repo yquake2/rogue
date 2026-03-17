b930131b79e023c5883ea538932cbf8fc65c3b86
BjossiAlfreds <bjornalfredsson@yahoo.com>
2026-03-17 12:14:23 +0000

    game: GetWeaponAmmoIndex function

--- src/g_items.c
+++ src/g_items.c
@@ -60,6 +60,22 @@ GetItemByIndex(int index)
 	return &itemlist[index];
 }
 
+int
+GetWeaponAmmoIndex(const gitem_t *weap)
+{
+	if (weap && weap->ammo)
+	{
+		const gitem_t *ammo = FindItem(weap->ammo);
+
+		if (ammo)
+		{
+			return ITEM_INDEX(ammo);
+		}
+	}
+
+	return 0;
+}
+
 gitem_t *
 FindItemByClassname(char *classname)
 {
